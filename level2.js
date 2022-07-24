e6c4121c94e2eaf82eb4d207ca3e7ed24865139d
prashant1110 <83967184+prashant1110@users.noreply.github.com>
2022-07-24 18:18:26 +0530

    Add files via upload

---
+++ level2.js
@@ -0,0 +1,32 @@
+//1.Using console.log() print out the following statement:
+
+let stat1="The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another."
+console.log(stsat1);
+
+//2.Using console.log() print out the following quote by Mother Teresa:
+
+let mt="Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
+console.log(mt);
+
+//3.Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
+
+let num1='10';
+let num2=10;
+
+if(num1===num2){
+    console.log("its true");
+}else
+{
+console.log(parseInt(num1));
+}
+
+//4.Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
+
+let num3=parseFloat("9.8");
+let num4=10;
+
+if(num3===mum4){
+    console.log("its true");
+}else{
+    console.log(parseInt(num3));
+}
